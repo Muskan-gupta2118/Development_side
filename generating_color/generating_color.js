042771c6a3ab11ebdb11a41c1836ae377889493f
Muskan Gupta <muskangupta2118@gmail.com>
2026-06-12 22:19:30 +0530

    generating_color.js

---
+++ generating_color/generating_color.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector("button");
+ console.dir(btn);
+
+ 
+
+ btn.addEventListener("click",function(){
+   let h1 = document.querySelector("h1");
+   let randomcolor = getRandomcolor();
+   h1.innerText = randomcolor;
+    let div=document.querySelector("div");
+    div.style.backgroundColor=randomcolor;
+    
+   console.log("color updated");
+});
+
+function getRandomcolor(){
+   let Red = Math.floor(Math.random()*255);
+   let green = Math.floor(Math.random()*255);
+   let blue = Math.floor(Math.random()*255);
+   let color =`rgb(${Red}, ${green}, ${blue})`;
+   return color;   
+}
+
+
+
+
